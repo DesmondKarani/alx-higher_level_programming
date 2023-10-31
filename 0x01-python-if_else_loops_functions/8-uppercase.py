2c0ccdfcae3c04bdb123891e9fd441660b1e6f25
Desmond <karanidesmond@protonmail.ch>
2023-10-31 20:27:27 +0300

    8-uppercase.py

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python3
 
 def uppercase(str):
-    result = ""
     for char in str:
-        if 97 <= ord(char) <= 122:  # ASCII values for lowercase letters
-            result += chr(ord(char) - 32)  # Convert to uppercase
+        if ord(char) >= 97 and ord(char) <= 122:  # ASCII values for lowercase letters
+            print(chr(ord(char) - 32), end='')  # Convert to uppercase
         else:
-            result += char  # Leave non-lowercase characters unchanged
-    print(result)
-
-# You can test your function with:
-# uppercase('hello world!')  # Should print: HELLO WORLD!
+            print(char, end='')  # Non-lowercase characters remain the same
+    print()  # New line
