7834f282013324677ced2e7863a276e6ed85d124
Desmond <karanidesmond@protonmail.ch>
2023-10-31 21:05:26 +0300

    3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
-for i in range(97, 123):
-    if chr(i) not in ['q', 'e']:
-        print("%c" % i, end='')
+for i in range(97, 123):  # ASCII values of lowercase letters
+    if chr(i) != 'q' and chr(i) != 'e':
+        print("{}".format(chr(i)), end='')
