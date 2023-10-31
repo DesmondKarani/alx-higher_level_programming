02d920d040cf8bf2f42a3be48baa9aee4b541779
Desmond <karanidesmond@protonmail.ch>
2023-10-31 20:24:23 +0300

    7-islower.py

---
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def islower(c):
+    return 97 <= ord(c) <= 122
+
+# You can test your function with:
+# print(islower('a'))  # Should print: True
+# print(islower('A'))  # Should print: False
