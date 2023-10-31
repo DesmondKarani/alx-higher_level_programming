c27354cf80567566fd14fa334aba8866636496da
Desmond <karanidesmond@protonmail.ch>
2023-10-31 21:15:33 +0300

    101-remove_char_at.py

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
-
 def remove_char_at(str, n):
     if n < 0 or n >= len(str):  # Check if n is out of bounds
         return str  # Return the original string if n is out of bounds
-    return str[:n] + str[n+1:]  # Concatenate the parts of the string before and after index n
+    elif n == 0:
+        return str[1:]  # Return the string without the first character
+    else:
+        return str[:n] + str[n+1:]  # Concatenate parts before n after index n
 
 # Example Usage:
-result = remove_char_at("Hello, World!", 7)
-print(result)  # Output: "Hello World!"
+result = remove_char_at("Hello, World!", 0)
+print(result)  # Output: "ello, World!"
