8c5bc4d02721d216585fa37c54afd1fb28cd1679
Desmond <karanidesmond@protonmail.ch>
2023-10-31 20:43:46 +0300

    9-print_last_digit.py

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 def print_last_digit(number):
-    last_digit = abs(number) % 10  # Get the last digit and handle negative numbers
-    print("{}".format(last_digit), end='')  # Print the last digit without a new line
+    last_digit = abs(number) % 10  # Get last digit and handle negative numbers
+    print("{}".format(last_digit), end='')  # Print last digit without new line
     return last_digit  # Return the value of the last digit
 
 # You can test your function with:
