d82e68d8c39631f350529df8c53171af09250237
Desmond <karanidesmond@protonmail.ch>
2023-11-02 20:46:03 +0300

    args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,9 +6,9 @@ def main():
 
     # Subtract 1 to exclude counting script name👇🏿
     num_args = len(sys.argv) - 1
-    # Include the script name in the count 👇🏿
-    print(f"{num_args + 1} arguments:" if num_args != 1 else "1 argument:")
-    for i, arg in enumerate(sys.argv, start=1):  # Start counting from 1
+    # Do not include the script name in the count 👇🏿
+    print(f"{num_args} argument(s):" if num_args != 1 else "1 argument:")
+    for i, arg in enumerate(sys.argv[1:], start=1):  # Start counting from 1
         print(f"{i}: {arg}")
 
 
