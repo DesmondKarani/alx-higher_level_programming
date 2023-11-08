e9788f2bc2e250d0fe8ec5ff83462676b2c60853
Desmond <karanidesmond@protonmail.ch>
2023-11-08 21:32:23 +0300

    add

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,22 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    if not matrix or not matrix[0]:
-        return []
-
-    # Create a new matrix with squared values using list comprehension
-    return [[element ** 2 for element in row] for row in matrix]
+    # Check if matrix is a valid 2D array
+    if not isinstance(matrix, list) or \
+            not all(isinstance(row, list) for row in matrix):
+        return None
+    # Create a new matrix with the same size as the input matrix
+    new_matrix = []
+    for row in matrix:
+        new_row = []
+        for element in row:
+            # Check if element is an integer
+            if isinstance(element, int):
+                # Square the element and append it to the new row
+                new_row.append(element ** 2)
+            else:
+                # Return None if element is not an integer
+                return None
+        # Append the new row to the new matrix
+        new_matrix.append(new_row)
+    # Return the new matrix
+    return new_matrix
