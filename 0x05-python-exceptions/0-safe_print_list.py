86dd81fa0c063acf80d0cfbaa17098e8b3aa4b3a
Desmond <karanidesmond@protonmail.ch>
2023-11-21 20:43:18 +0300

    new line

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,23 +1,32 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
+    # Initialize a counter for the number of printed elements
     count = 0
-    for i in range(x):
-        try:
-            print(my_list[i], end='')
+    # Use a try-except block to handle possible IndexError
+    try:
+        # Loop through the list from index 0 to x - 1
+        for i in range(x):
+            # Print the element at index i
+            print(my_list[i], end="")
+            # Increment the counter
             count += 1
-        except IndexError:
-            break
-    print()
+        # Print a new line after the loop
+        print()
+    except IndexError:
+        # If an IndexError occurs, print a new line
+        print()
+    # Return the number of printed elements
     return count
 
-# Test cases
-if __name__ == "__main__":
-    my_list = [1, 2, 3, 4, 5]
-
-    nb_print = safe_print_list(my_list, 2)
-    print("nb_print: {:d}".format(nb_print))
-    nb_print = safe_print_list(my_list, 5)
-    print("nb_print: {:d}".format(nb_print))
-    nb_print = safe_print_list(my_list, 7)
-    print("nb_print: {:d}".format(nb_print))
+# Test the function with different values of x
+my_list = [1, 2, 3, 4, 5]
+x = 3
+print("Output for x =", x)
+safe_print_list(my_list, x)
+x = 5
+print("Output for x =", x)
+safe_print_list(my_list, x)
+x = 7
+print("Output for x =", x)
+safe_print_list(my_list, x)
