8f37301041682b6e5c25b5137954003713d7bbfc
Desmond <karanidesmond@protonmail.ch>
2023-11-29 19:16:38 +0300

    a rectangle class

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python3
+"""a class Rectangle that defines a rectangle"""
+
+
+class Rectangle:
+    """Class that defines a rectangle."""
+
+    def __init__(self, width=0, height=0):
+        """Initialize a new Rectangle.
+
+        Args:
+            width (int): The width of the rectangle.
+            height (int): The height of the rectangle.
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Get or set the width of the rectangle."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Get or set the height of the rectangle."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """Return the area of the rectangle."""
+        return self.width * self.height
+
+    def perimeter(self):
+        """Return the perimeter of the rectangle."""
+        if self.width == 0 or self.height == 0:
+            return 0
+        return 2 * (self.width + self.height)
+
+    def __str__(self):
+        """Return the printable representation of the rectangle.
+
+        The rectangle is represented by a grid of '#' characters.
+        """
+        if self.width == 0 or self.height == 0:
+            return ""
+
+        rectangle_str = []
+        for _ in range(self.height):
+            rectangle_str.append("#" * self.width)
+        return "\n".join(rectangle_str)
