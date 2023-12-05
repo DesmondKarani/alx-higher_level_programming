796b46fdc19e84b9ab6edffadda1033cc5375d0c
Desmond <karanidesmond@protonmail.ch>
2023-12-05 20:10:58 +0300

    py py

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""a class Student that defines a student"""
+
+
+class Student:
+    """Defines a student"""
+
+    def __init__(self, first_name, last_name, age):
+        """Instantiates a Student object"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieves a dictionary representation of a Student instance"""
+        if attrs is None:
+            return self.__dict__
+        return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""a class Student that defines a student"""
+
+
+class Student:
+    """Defines a student"""
+
+    def __init__(self, first_name, last_name, age):
+        """Instantiates a Student object"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieves a dictionary representation of a Student instance"""
+        if attrs is None:
+            return self.__dict__
+        return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+
+    def reload_from_json(self, json):
+        """Replaces all attributes of the Student instance"""
+        for key, value in json.items():
+            setattr(self, key, value)
