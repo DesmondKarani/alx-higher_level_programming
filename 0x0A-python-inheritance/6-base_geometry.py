9264734a8460388d484860bd74bb0abea0a95042
Desmond <karanidesmond@protonmail.ch>
2023-12-05 19:25:22 +0300

    pp

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 """a class BaseGeometry"""
 
+
 class BaseGeometry:
-    """ An enhanced BaseGeometry class """
+    """An enhanced BaseGeometry class."""
 
     def area(self):
-        """ Raises an Exception with the message that the
-        method is not implemented """
+        """Raises an Exception with the message that the
+        method is not implemented"""
         raise Exception("area() is not implemented")
