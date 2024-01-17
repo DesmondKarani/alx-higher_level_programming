563dca8a0762cddc8298ca2eb0bba08c3cc4dd3b
Desmond <karanidesmond@protonmail.ch>
2024-01-17 21:42:53 +0300

    tables

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,5 +1,5 @@
 -- Script to display top 3 cities with highest average temperature in July and August
-SELECT city, AVG(temperature) AS avg_temp 
+SELECT city, AVG(value) AS avg_temp 
 FROM temperatures 
 WHERE MONTH(record_date) IN (7, 8)  -- 7 and 8 represent July and August
 GROUP BY city 
