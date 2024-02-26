560a539256f4a68a2784a88c083ef06980fe4d66
Desmond <karanidesmond@protonmail.ch>
2024-02-26 11:35:11 +0300

    corrections

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -10,16 +10,14 @@ import MySQLdb
 import sys
 
 
-db = MySQLdb.connect(host="localhost", user=sys.argv[1],
-                     passwd=sys.argv[2], db=sys.argv[3], port=3306)
-cur = db.cursor()
-cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'"
-            .format(sys.argv[4]))
-rows = cur.fetchall()
-for row in rows:
-    print(row)
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'"
+                .format(sys.argv[4]))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
     cur.close()
     db.close()
-
-if __name__ == "__main__":
-    main()
