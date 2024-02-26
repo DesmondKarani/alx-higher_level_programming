01e9a07170231980a59db2461df1b3d5f7e79bc3
Desmond <karanidesmond@protonmail.ch>
2024-02-26 12:39:23 +0300

    aii

---
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" the class attribute cities must represent a relationship with the class
+    City.
+If the State object is deleted, all linked City objects must
+    be automatically deleted."""
+
+from sqlalchemy import Column, Integer, String, MetaData
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
+mymetadata = MetaData()
+Base = declarative_base(metadata=mymetadata)
+
+
+class State(Base):
+    """
+    Class with id and name attributes of each state
+    """
+    __tablename__ = 'states'
+    id = Column(Integer, unique=True, nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="states")
