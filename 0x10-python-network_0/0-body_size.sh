fd33b21ed299d247c34ab46c58e15b727e968ef4
Desmond <karanidesmond@protonmail.ch>
2024-02-29 18:14:40 +0300

    ai

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# This script takes a URL as an argument and displays the size of the body of the response
-curl -s -w '%{size_download}\n' "$1"
+# This script sends a request to a URL and displays the size of the body of the response
+curl -sI "$1" | grep Content-Length | awk '{print $2}'
