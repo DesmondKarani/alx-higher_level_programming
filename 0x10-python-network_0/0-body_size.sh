a8d8424ef3fea3494aa33ea68b9ddc8223dc95c2
Desmond <karanidesmond@protonmail.ch>
2024-02-29 18:03:56 +0300

    shorten

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-# This script takes a URL as an argument and displays the size of the response body in bytes
-[ $# -eq 0 ] && echo "Usage: $0 URL" && exit 1 || echo $(curl -s -w "%{size_download}" -o /dev/null "$1")
+# This script takes a URL as an argument and displays the size of the body of the response
+curl -s -w '%{size_download}\n' "$1"
 
