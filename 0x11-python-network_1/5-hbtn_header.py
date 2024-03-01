9d3498eb2eaafe261868a29c2ac5af82820d3bf4
Desmond <karanidesmond@protonmail.ch>
2024-03-01 21:20:32 +0300

    maswali

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
-"""a Python script that takes in a URL,
-sends a request to the URL
-and displays the value of the variable X-Request-Id in the response header"""
+""" a Python script that takes in a URL,
+sends a request to the URL and
+displays the value of the variable X-Request-Id in the response header"""
 
 
 import requests
+import sys
 
 if __name__ == "__main__":
-    url = 'https://alx-intranet.hbtn.io/status'
-    # Send a GET request to the URL
+    # Get the URL from the command line argument
+    url = sys.argv[1]
+    # Send a request to the URL
     response = requests.get(url)
-    print("Body response:")
-    # Print the type of the response text
-    print("\t- type:", type(response.text))
-    # Print the content of the response
-    print("\t- content:", response.text)
+    # Get the value of the X-Request-Id header
+    x_request_id = response.headers.get("X-Request-Id")
+    # Print the value
+    print(x_request_id)
